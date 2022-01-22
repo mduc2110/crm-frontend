cf57cc1e5860880e38f84e7b60710f3006a5d542
Duc <vmd211099@gmail.com>
2022-01-22 13:44:55 +0700

    add littie css lol

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineMail, AiOutlineUser } from "react-icons/ai";
+import { AiOutlineAppstore, AiOutlineMail, AiOutlineStock, AiOutlineUser } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
 import classes from "./sidebar.module.css";
@@ -8,14 +8,27 @@ const Sidebar = () => {
       <div className={classes.sidebar}>
          <ul>
             <li>
-               <AiOutlineMail /> <NavLink to="/customer">Customer</NavLink>
+               <NavLink to="/dashboard" className={"active"}>
+                  {/* <NavLink className={({ isActive }) => (isActive ? "active" : "")} */}
+                  <AiOutlineAppstore /> Dashboard
+               </NavLink>
             </li>
             <li>
-               <AiOutlineUser />
-               <NavLink to="/user">User</NavLink>
+               <NavLink to="/customer">
+                  <AiOutlineMail /> Khách hàng
+               </NavLink>
             </li>
             <li>
-               <AiOutlineMail />{" "}
+               <NavLink to="/user">
+                  <AiOutlineUser />
+                  Nhân viên
+               </NavLink>
+            </li>
+            <li>
+               <NavLink to="/statistic">
+                  <AiOutlineStock />
+                  Thống kê
+               </NavLink>
             </li>
             <li>
                <AiOutlineMail />{" "}
